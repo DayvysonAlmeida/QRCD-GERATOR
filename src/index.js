754a1acada1a11aaeb19bb20357c7a8df8779049
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2023-07-19 22:54:52 -0300

    add const document.queryselector

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper");
+const form = document.querySelectorAll("form");
+const fileInp = document.querySelectorAll("input");
+const infoText = document.querySelectorAll("p");
+const closeBtn = document.querySelectorAll(".close");
+const copyBtn = document.querySelectorAll(".copy");
