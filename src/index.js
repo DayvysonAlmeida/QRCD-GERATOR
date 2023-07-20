33d63b4e5aff517173092d5935c46c15f64aad47
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2023-07-20 00:00:45 -0300

    alterações NA FUNCTION FETCHREQQUEST

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ function fetchRequest(file, formData){
     infoText.innerText = "Scanning QR Code...";
     fetch("http://api.qrserver.com/v1/read-qr-code/",{
         method: 'POST', body: formData
-    }).then(res=> res.json()).then(result =>{
-        result =[0].symbol[0].data;
-        inforText.innerText = result ? "Upload QR Code To Scan" : "Couldn't Scan QR Code";
+    }).then(res => res.json()).then(result =>{
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "Upload QR Code To Scan" : "Couldn't Scan QR Code";
         if (!result) return;
         document.querySelector("textarea").innerText = result;
         form.querySelector("img").src = URL.createObjectURL(file);
